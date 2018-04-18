12c71901f63894e236e58f4efe1fa09f3158244d
J-Rios <jose_23991@hotmail.com>
2018-04-18 23:50:45 +0200

    Support for allow/deny users to invite/add Bots to groups

--- sources/Anti_Join2Spam_Bot.py
+++ sources/Anti_Join2Spam_Bot.py
@@ -10,9 +10,9 @@ Author:
 Creation date:
     04/04/2018
 Last modified date:
-    14/04/2018
+    18/04/2018
 Version:
-    1.3.1
+    1.4.0
 '''
 
 ####################################################################################################
@@ -164,7 +164,8 @@ def get_default_config_data():
         ('Antispam', CONST['INIT_ENABLE']), \
         ('Time_for_allow_urls_h', CONST['INIT_TIME_ALLOW_URLS']), \
         ('Num_messages_for_allow_urls', CONST['INIT_MIN_MSG_ALLOW_URLS']), \
-        ('Call_admins_when_spam_detected', CONST['INIT_CALL_ADMINS_WHEN_SPAM']) \
+        ('Call_admins_when_spam_detected', CONST['INIT_CALL_ADMINS_WHEN_SPAM']), \
+        ('Allow_users_to_add_bots', CONST['INIT_ALLOW_USERS_ADD_BOTS'])
     ])
     return config_data
 
@@ -307,10 +308,41 @@ def get_admins_usernames_in_string(bot, chat_id):
 def new_user(bot, update):
     '''New member join the group event handler'''
     chat_id = update.message.chat_id
-    user_id = update.message.new_chat_member.id
-    user_name = update.message.new_chat_member.name
+    msg_from_user_id = update.message.from_user.id
+    msg_from_user_name = update.message.from_user.name
     join_date = (update.message.date).now().strftime("%Y-%m-%d %H:%M:%S")
-    register_new_user(chat_id, user_id, user_name, join_date)
+    # For each new user that join or has been added
+    for join_user in update.message.new_chat_members:
+        join_user_id = join_user.id
+        join_user_name = join_user.name
+        # If the added user is not myself (this Bot)
+        if bot.id != join_user_id:
+            register_user = True
+            # If the message user source is not the join user, it has been invited/added by another
+            if msg_from_user_id != join_user_id:
+                # If the member that has been join the group is a Bot
+                if update.message.new_chat_members[0].is_bot:
+                    # If the user that has been added the Bot is not an Admin
+                    if not user_is_admin(bot, msg_from_user_id, chat_id):
+                        # If not allow users to add Bots
+                        if get_chat_config(chat_id, 'Allow_users_to_add_bots') == False:
+                            # Kick the Added Bot and notify
+                            bot.kickChatMember(chat_id, join_user_id)
+                            lang = get_chat_config(chat_id, 'Language')
+                            call_admins_when_spam_detected = get_chat_config(chat_id, \
+                                'Call_admins_when_spam_detected')
+                            bot_msg_1 = TEXT[lang]['USER_CANT_ADD_BOT'].format(msg_from_user_name, \
+                                join_user_name)
+                            bot_message = bot_msg_1
+                            if call_admins_when_spam_detected:
+                                admins = get_admins_usernames_in_string(bot, chat_id)
+                                if admins:
+                                    bot_msg_2 = TEXT[lang]['CALLING_ADMINS'].format(admins)
+                                    bot_message = "{}{}".format(bot_message, bot_msg_2)
+                            bot.send_message(chat_id, bot_message)
+                            register_user = False
+            if register_user:
+                register_new_user(chat_id, join_user_id, join_user_name, join_date)
 
 
 def msg_nocmd(bot, update):
@@ -511,8 +543,9 @@ def cmd_status(bot, update):
     num_messages_for_allow_urls = get_chat_config(chat_id, 'Num_messages_for_allow_urls')
     time_for_allow_urls_h = get_chat_config(chat_id, 'Time_for_allow_urls_h')
     call_admins_when_spam_detected = get_chat_config(chat_id, 'Call_admins_when_spam_detected')
+    allow_users_to_add_bots = get_chat_config(chat_id, 'Allow_users_to_add_bots')
     bot_msg = TEXT[lang]['STATUS'].format(num_messages_for_allow_urls, time_for_allow_urls_h, \
-        call_admins_when_spam_detected, enable)
+        call_admins_when_spam_detected, allow_users_to_add_bots, enable)
     tlg_msg_to_selfdestruct(bot, update.message)
     tlg_send_selfdestruct_msg(bot, chat_id, bot_msg)
 
@@ -527,7 +560,7 @@ def cmd_call_admins(bot, update):
 
 
 def cmd_call_when_spam(bot, update, args):
-    '''Command /cmd_call_when_spam message handler'''
+    '''Command /call_when_spam message handler'''
     chat_id = update.message.chat_id
     user_id = update.message.from_user.id
     lang = get_chat_config(chat_id, 'Language')
@@ -565,8 +598,47 @@ def cmd_call_when_spam(bot, update, args):
     tlg_send_selfdestruct_msg(bot, chat_id, bot_msg)
 
 
+def cmd_users_add_bots(bot, update, args):
+    '''Command /users_add_bots message handler'''
+    chat_id = update.message.chat_id
+    user_id = update.message.from_user.id
+    lang = get_chat_config(chat_id, 'Language')
+    allow_users_to_add_bots = get_chat_config(chat_id, 'Allow_users_to_add_bots')
+    is_admin = user_is_admin(bot, user_id, chat_id)
+    if is_admin == True:
+        if len(args) == 1:
+            value_provided = args[0]
+            if value_provided == 'enable' or value_provided == 'disable':
+                if value_provided == 'enable':
+                    if allow_users_to_add_bots == True:
+                        bot_msg = TEXT[lang]['USERS_ADD_BOTS_ALREADY_ENABLE']
+                    else:
+                        bot_msg = TEXT[lang]['USERS_ADD_BOTS_ENABLE']
+                        allow_users_to_add_bots = True
+                        save_config_property(chat_id, 'Allow_users_to_add_bots', \
+                            allow_users_to_add_bots)
+                else:
+                    if allow_users_to_add_bots == True:
+                        bot_msg = TEXT[lang]['USERS_ADD_BOTS_DISABLE']
+                        allow_users_to_add_bots = False
+                        save_config_property(chat_id, 'Allow_users_to_add_bots', \
+                            allow_users_to_add_bots)
+                    else:
+                        bot_msg = TEXT[lang]['USERS_ADD_BOTS_ALREADY_DISABLE']
+            else:
+                bot_msg = TEXT[lang]['USERS_ADD_BOTS_NOT_ARG']
+        else:
+            bot_msg = TEXT[lang]['USERS_ADD_BOTS_NOT_ARG']
+    elif is_admin == False:
+        bot_msg = TEXT[lang]['CMD_NOT_ALLOW']
+    else:
+        bot_msg = TEXT[lang]['CAN_NOT_GET_ADMINS']
+    tlg_msg_to_selfdestruct(bot, update.message)
+    tlg_send_selfdestruct_msg(bot, chat_id, bot_msg)
+
+
 def cmd_enable(bot, update):
-    '''Command /disable message handler'''
+    '''Command /enable message handler'''
     chat_id = update.message.chat_id
     user_id = update.message.from_user.id
     lang = get_chat_config(chat_id, 'Language')
@@ -619,7 +691,7 @@ def cmd_version(bot, update):
 
 
 def cmd_about(bot, update):
-    '''Command /cmd_about handler'''
+    '''Command /about handler'''
     chat_id = update.message.chat_id
     lang = get_chat_config(chat_id, 'Language')
     bot_msg = TEXT[lang]['ABOUT_MSG'].format(CONST['DEVELOPER'], CONST['REPOSITORY'])
@@ -712,6 +784,7 @@ def main():
     dp.add_handler(CommandHandler("status", cmd_status))
     dp.add_handler(CommandHandler("call_admins", cmd_call_admins))
     dp.add_handler(CommandHandler("call_when_spam", cmd_call_when_spam, pass_args=True))
+    dp.add_handler(CommandHandler("users_add_bots", cmd_users_add_bots, pass_args=True))
     dp.add_handler(CommandHandler("enable", cmd_enable))
     dp.add_handler(CommandHandler("disable", cmd_disable))
     dp.add_handler(CommandHandler("version", cmd_version))
